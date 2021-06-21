e80b2e5173f5a024d1d79212146a50a4c6f24843
emregecit2 <emregecit2@gmail.com>
2021-06-21 22:01:52 +0300

    Update the2.py

--- ceng111/the2/the2.py
+++ ceng111/the2/the2.py
@@ -7,7 +7,7 @@ def new_move():
 	for y in range(len(universal_state)):#this loop applies the movement rules
 		tv=universal_state[y][:] #tv: temporary value
 		tv[1]-=random.choices([8,7,6,5,4,3,2,1],weights=[1/2*MU,1/8*MU,1/2*(1-MU-MU**2),2/5*MU**2,1/5*MU**2,2/5*MU**2,1/2*(1-MU-MU**2),1/8*MU])[0] #if random.choices returns [2], it means left. left changes your direction 6 units in counter clockwise direction. others are similar.if we substract that from tv[1], we can get the direction we want.
-		tv[1]=[0,1,2,3,4,5,6,7][tv[1]] # i'm using negative indexing property of lists because if tv[1]=-3 it is actally 5.
+		tv[1]=[0,1,2,3,4,5,6,7][tv[1]] # i'm using negative indexing property of lists because if tv[1]=-3 it is actually 5.
 		lst_coded=[[0,1],[-1,1],[-1,0],[-1,-1],[0,-1],[1,-1],[1,0],[1,1]] #I defined a coded list which encodes "where to move" data in [x,y] form. If x is positive that means x will increase by one.
 		tv[0]=(tv[0][0]+lst_coded[tv[1]][0],tv[0][1]+lst_coded[tv[1]][1])
 		for controller in universal_state: #after here, i check if there is any other indivudual where we want to go. if there is no individual, we can finally go there.
